bdfd2068bd857537a8b6cfc1d46285289ee328a3
deepak <deepak@webexpertinfotech.com>
2023-10-09 18:43:29 +0530

    experience section

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -6,14 +6,15 @@ const Experience = () => {
             <div className="container-xl">
                 <h3 className="section-title mb-16">Experience</h3> 
                 <div className="flex flex-wrap gap-4">
-                    <div className="lg:w-2/4 p-1 flex flex-col rounded-lg bg-slate-700 hover:bg-yellow-500">
-                        <img src={weiLogo} alt="man computer" />
-                        
+                    <div className="lg:w-1/3 flex flex-col rounded-lg bg-slate-700 hover:bg-yellow-500 shadow-md shadow-black">
+                        <div className="h-52 overflow-hidden">
+                            <img src={weiLogo} alt="man computer" className="h-full"  />                        
+                        </div>
                         <div className="text-white mb-5 p-5">
-                            <h3 className=" font-bold text-3xl mb-5">WebexpertInfotech</h3>
-                            <p className="text-sm font-bold mt-1 "><span className="font-bold">Duration</span>: 2019-Present</p>
-                            <p className="text-sm font-bold mt-1 "><span className="font-bold">Role</span>: Software Developer</p>
-                            <p className="text-sm font-bold mt-1 "><span className="font-bold">Position</span>: Project Manager</p>
+                            <h3 className=" text-3xl mb-5">WebexpertInfotech</h3>
+                            <p className="text-md mt-1 flex justify-items-start gap-10"><span className="font-bold">Duration:</span> 2019-Present</p>
+                            <p className="text-md mt-1 flex justify-items-start gap-10 "><span className="font-bold mr-8">Role:</span> Software Developer</p>
+                            <p className="text-md mt-1 flex justify-items-start gap-10"><span className="font-bold mr-1">Position:</span> Project Manager</p>
                         </div>
                     </div>
                 </div>
